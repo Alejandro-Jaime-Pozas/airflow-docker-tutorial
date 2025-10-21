fcbb09cc951c093e413d4506342214a140e9aaba
Alex <ajaimepozas@gmail.com>
2025-10-21 13:32:24 -0600

    working on postgres connection

---
+++ dags/dag_with_catchup_backfill.py
@@ -0,0 +1,16 @@
+# from datetime import datetime, timedelta
+
+# from airflow import DAG
+# from airflow.providers.standard.operators.bash import BashOperator
+
+
+# default_args = {
+#     'owner': 'alex',
+#     'retries': 5,
+#     'retry_delay': timedelta(minutes=5),
+# }
+
+
+# with DAG(
+
+# ) as dag():
